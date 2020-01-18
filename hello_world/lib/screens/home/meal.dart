609d0da9c982c262ea5442f0f8695610f84138ba
Scott Williams <scotty2@cogeco.ca>
2020-01-18 11:41:59 -0500

    Dropdown menu for Yousaf

--- hello_world/lib/screens/home/meal.dart
+++ hello_world/lib/screens/home/meal.dart
@@ -19,51 +19,65 @@ class Meal extends StatelessWidget {
 
   Widget _buildAddFoodDropdown() {
     return Container(
-      padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 5),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(15.0),
-        border: Border.all(
-            color: Colors.blueGrey, style: BorderStyle.solid, width: 2),
-      ),
-      child: DropdownButton<String>(
-        hint: Row(
-          children: <Widget>[
-            Icon(Icons.add, color: Colors.grey),
-            Text("    Add a Food")
-          ],
-        ),
-        icon: Icon(Icons.arrow_downward),
-        iconSize: 24,
-        elevation: 16,
-        style: TextStyle(color: Colors.black),
-        onChanged: (String newValue) {
-          if (newValue == "Camera") {
-            //
-          } else if (newValue == "Lookup") {
-          } else if (newValue == "Barcode") {}
-        },
-        items: [
-          DropdownMenuItem(
-            value: "Camera",
-            child: Row(
-              children: <Widget>[Icon(Icons.photo_camera), Text("   Camera")],
-            ),
-          ),
-          DropdownMenuItem(
-            value: "Lookup",
-            child: Row(
-              children: <Widget>[Icon(Icons.search), Text("   Search")],
-            ),
-          ),
-          DropdownMenuItem(
-            value: "Barcode",
-            child: Row(
-              children: <Widget>[
-                Icon(Icons.camera_enhance),
-                Text("   Barcode")
-              ],
-            ),
-          )
+      padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 15),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15.0),
+                color: Colors.red,
+                border: Border.all(
+                    color: Colors.lightBlue, style: BorderStyle.solid, width: 2),
+              ),
+              child: DropdownButton<String>(
+                hint: Row(
+                  children: <Widget>[
+                    Icon(Icons.add, color: Colors.white),
+                    Text("    Add a Food",
+                        style: TextStyle(color: Colors.white))
+                  ],
+                ),
+                icon: Icon(Icons.arrow_downward, color: Colors.white),
+                iconSize: 24,
+                elevation: 16,
+                style: TextStyle(color: Colors.black),
+                onChanged: (String newValue) {
+                  if (newValue == "Camera") {
+                    // Go to camera screen
+                  } else if (newValue == "Lookup") {
+                    // Go to search screen
+                  } else if (newValue == "Barcode") {
+                    // Go to barcode screen
+                  }
+                },
+                items: [
+                  DropdownMenuItem(
+                    value: "Camera",
+                    child: Row(
+                      children: <Widget>[
+                        Icon(Icons.photo_camera),
+                        Text("   Camera")
+                      ],
+                    ),
+                  ),
+                  DropdownMenuItem(
+                    value: "Lookup",
+                    child: Row(
+                      children: <Widget>[Icon(Icons.search), Text("   Search")],
+                    ),
+                  ),
+                  DropdownMenuItem(
+                    value: "Barcode",
+                    child: Row(
+                      children: <Widget>[
+                        Icon(Icons.camera_enhance),
+                        Text("   Barcode")
+                      ],
+                    ),
+                  )
+                ],
+              )),
         ],
       ),
     );
