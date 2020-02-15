dd683de02a58d6905649da8a174643b196a1373f
Scott Williams <scotty2@cogeco.ca>
2020-02-14 22:43:43 -0500

    UserSettings stuff

---
+++ hello_world/lib/models/userSettings.dart
@@ -0,0 +1,10 @@
+class UserSettings {
+  final String uid;
+  final DateTime dateOfBirth;
+  final String firstName; 
+  final String lastName; 
+  final String userName;
+  final double height;
+
+  UserSettings({this.uid, this.dateOfBirth, this.firstName, this.lastName, this.userName, this.height});
+}
